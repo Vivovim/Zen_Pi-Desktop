1ac01af02889bf4cdc08e0475843cbf33272c3f9
Christopher Huffaker <neo@ctopher.me>
2024-11-16 16:17:38 -0700

    Initial Commit

---
+++ Testing/TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingApp.swift
+//  Testing
+//
+//  Created by Christopher Huffaker on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
